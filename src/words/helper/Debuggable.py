adeece32683cefc6da5d62f928e4ab0559665015
David <david.strootman@gmail.com>
2021-08-18 13:37:48 +0200

    Add docstring for debuggable

--- src/words/helper/Debuggable.py
+++ src/words/helper/Debuggable.py
@@ -4,4 +4,4 @@ from abc import ABC, abstractmethod
 class Debuggable(ABC):
     @abstractmethod
     def debug_str(self):
-        pass
+        """A debug string is used for providing better error messages during both parsing and at runtime."""
