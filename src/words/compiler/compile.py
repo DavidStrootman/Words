c734d7c61175f13d2ef46875ee635f982f068d94
David Strootman <david.strootman@gmail.com>
2022-04-08 18:10:13 +0200

    Add compilation support for more tokens

--- src/words/compiler/compile.py
+++ src/words/compiler/compile.py
@@ -8,7 +8,7 @@ from words.parser.parse import Parser
 from words.parser.parse_util import Program
 from words.token_types.lexer_token import LexerToken
 from words.token_types.parser_token import ParserToken, VariableParserToken, FunctionParserToken, NumberParserToken, \
-    IdentParserToken, MacroParserToken, BooleanOperatorParserToken
+    IdentParserToken, MacroParserToken, BooleanOperatorParserToken, IfParserToken, ReturnParserToken
 
 
 class Compiler:
@@ -74,28 +74,35 @@ class M0Compiler:
     @staticmethod
     def _compile_function_token(function_token: FunctionParserToken) -> str:
         output = ""
-        param_regs = {i + 4: prm.value for i, prm in enumerate(function_token.parameters)}
-        # Reserve parameters
+        func_end = function_token.name + "_end"
+        output += f"b {func_end}\n"
+        output += f"{function_token.name}:\n"
 
+        # Reserve parameters
+        param_regs = {i + 4: prm.value for i, prm in enumerate(function_token.parameters)}
         output = output + m0.asm_instruction_list("pop", list(param_regs.keys()))
 
         for token in function_token.body:
-            M0Compiler._compile_token(token, param_regs)
+            output += M0Compiler._compile_token(token, param_regs)
 
+        output += f"{func_end}:\n"
         return output
 
     @staticmethod
     def _compile_number_token(token) -> str:
-
-        return f"#{token.value}"
+        return m0.asm_push_value_stack(token.value)
 
     @staticmethod
     def _compile_ident_token(token, used_regs) -> str:
+        if token.value not in used_regs.values():
+            # It must be a function (or some undefined identifier)
+            return m0.asm_branch(token.value)
+        # It is a parameter or variable
         reg_to_push: List[int] = [m0.reg_from_val(token.value, used_regs)]
         return m0.asm_instruction_list("push", reg_to_push)
 
     @staticmethod
-    def _compile_boolean_op_token(token, used_regs) -> str:
+    def _compile_boolean_op_token(token) -> str:
         output = ""
 
         output += m0.asm_instruction_list("pop", [0, 1])
@@ -116,6 +123,14 @@ class M0Compiler:
         return ""
 
     @staticmethod
+    def _compile_if_token(token, used_regs) -> str:
+        return ""
+
+    @staticmethod
+    def _compile_return_token(token, used_regs) -> str:
+        return ""
+
+    @staticmethod
     def _compile_token(token: ParserToken, used_regs: dict) -> str:
         if isinstance(token, FunctionParserToken):
             return M0Compiler._compile_function_token(token)
@@ -126,7 +141,11 @@ class M0Compiler:
         elif isinstance(token, MacroParserToken):
             return M0Compiler._compile_macro_token(token)
         elif isinstance(token, BooleanOperatorParserToken):
-            return M0Compiler._compile_boolean_op_token(token, used_regs)
+            return M0Compiler._compile_boolean_op_token(token)
+        elif isinstance(token, IfParserToken):
+            return M0Compiler._compile_if_token(token, used_regs)
+        elif isinstance(token, ReturnParserToken):
+            return M0Compiler._compile_return_token(token, used_regs)
         raise RuntimeError(f"Cannot compile token {token.debug_str()}")
 
     @staticmethod
@@ -134,16 +153,13 @@ class M0Compiler:
         dot_global = (
             ".global setup, loop\n\n"
         )
-        code = []
+        code = ""
         for token in ast.tokens:
-            code = code + [M0Compiler._compile_token(token, {}), ]
-        code_segment = (
-            dot_global + "setup: \n"
+            code = code + M0Compiler._compile_token(token, {})
 
-                         "loop: \n"
-        )
+        setup = "setup: \n" + code
 
-        return code_segment
+        return dot_global + setup + "loop: \n"
 
     @staticmethod
     def compile(ast: Program):
--- src/words/compiler/compile_util.py
+++ src/words/compiler/compile_util.py
@@ -1,28 +1,46 @@
 class M0Util:
     @staticmethod
+    def asm_reg(reg: int):
+        return f"r{reg}"
+
+    @staticmethod
     def asm_instruction_load(dest_reg: int, immed32: int):
-        return f"ldr {dest_reg}, ={immed32}\n"
+        return f"ldr {M0Util.asm_reg(dest_reg)}, ={immed32}\n"
 
     @staticmethod
     def asm_instruction_move(dest_reg: int, immed8: int):
-        return f"mov {dest_reg}, #{immed8}\n"
+        return f"mov {M0Util.asm_reg(dest_reg)}, #{immed8}\n"
 
     @staticmethod
-    def asm_instruction_one_reg(inst: str, reg1: int, immed: int):
-        return f"{inst} {reg1}"
+    def asm_instruction_one_reg(inst: str, reg1: int):
+        return f"{inst} {M0Util.asm_reg(reg1)}"
 
     @staticmethod
     def asm_instruction_two_reg(inst: str, reg1: int, reg2: int):
-        return f"{inst} {reg1}, {reg2}"
+        return f"{inst} {M0Util.asm_reg(reg1)}, {M0Util.asm_reg(reg2)}"
 
     @staticmethod
     def asm_instruction_list(inst: str, regs: list[int]):
-        regs_str = ", ".join([f"r{str(reg)}" for reg in regs])
+        regs_str = ", ".join([M0Util.asm_reg(reg) for reg in regs])
         return f"{inst} {{{regs_str}}}\n"
 
     @staticmethod
+    def asm_push_value_stack(value: int) -> str:
+        output = ""
+        if value > 0x11111111:
+            output += M0Util.asm_instruction_load(0, value)
+        output += M0Util.asm_instruction_move(0, value)
+        output += M0Util.asm_instruction_list("push", [0])
+
+        return output
+
+    @staticmethod
+    def asm_branch(target: str) -> str:
+        return f"b {target}\n"
+
+    @staticmethod
     def reg_from_val(ident_val: str, used_regs: dict) -> int:
-        index = list(used_regs.values()).index(ident_val)
+        index = list(used_regs.values()).index(ident_val) + 4
         return index
 
     boolean_compile_map = {
